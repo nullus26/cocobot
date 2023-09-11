e791012f15ef70a9ba06e65d02152ce32238b523
nullus26 <juani.nullo06@gmail.com>
2023-09-10 23:40:26 -0300

    Changed how program works

--- bot.py
+++ bot.py
@@ -1,42 +1,24 @@
 import discord
 from discord.ext import commands
-import responses
+
+TOKEN = "."
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='/', intents=intents)
 
 
-async def send_message(message, user_message, is_private):
-    try:
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+@client.event
+async def on_ready():
+    print('bot is ready')
+
+@client.event
+async def on_message(message):
+    print(message.author, message.content, message.channel.id)
+
+@client.command()
+async def hello(ctx):
+    await client.send_message("hello")
 
 
-def run_discord_bot():
-    #no hermano consiguete la tuya :vvv
-    TOKEN = '.'
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
-
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now running')
-
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-
-        print(f"{username} said: '{user_message}' ({channel})")
-
-        if user_message[0] == '/':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=False)
-        
-    
-    client.run(TOKEN)
-    
+client.run(TOKEN)
